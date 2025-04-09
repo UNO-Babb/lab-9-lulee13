d40ae12fb55d4341f38feb98cff25582bb84f1b0
lulee13 <lucaslee@unomaha.edu>
2025-04-09 14:56:25 +0000

    done!

--- SteganographyApp.py
+++ SteganographyApp.py
@@ -97,6 +97,15 @@ def numberToBinary(num):
   """Takes a base10 number and converts to a binary string with 8 bits"""
   binary = ""
   #Convert from decimal to binary
+  value = 128
+  while num > 0 or len(binary) < 8:
+    if num >= value:
+      binary += "1"
+      num -= value
+    else:
+      binary += "0"
+
+    value //= 2
 
 
   return binary
@@ -104,22 +113,35 @@ def numberToBinary(num):
 def binaryToNumber(bin):
   """Takes a string binary value and converts it to a base10 integer."""
   decimal = 0
+  value = 128
+  for digit in bin:
+    if digit == "1":
+      decimal = decimal + value
+    
+    value = value//2
 
 
   return decimal
 
 def main():
   #Ask user if they want to encode/decode
-  myImg = Image.open('pki.png')
-  myMsg = "This is a secret message I will hide in an image."
-  encode(myImg, myMsg)
-  myImg.close()
+  mode = input("Type 'encode' if you would like to encode a message and type 'decode' if you would like to decode a message: ")
+  if mode == "encode":
+    myMsg = input("Type the message you would like to encode: ")
+    myImg = Image.open('pki.png')
+    #myMsg = "This is a secret message I will hide in an image."
+    encode(myImg, myMsg)
+    myImg.close()
+  
+  if mode == "decode":
+  
+    yourImg = Image.open('secretImg.png')
+    msg = decode(yourImg)
+    print(msg)
 
-  """
-  yourImg = Image.open('secretImg.png')
-  msg = decode(yourImg)
-  print(msg)
-  """
+  else: 
+    print ("ERROR")
+  
     
 if __name__ == '__main__':
   main()
